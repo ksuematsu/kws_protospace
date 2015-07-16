61396d1af71b6decf35af746b26f133ba665ef77
ksuematsu <ksuematsu68@gmail.com>
2015-07-16 22:32:05 +0900

    arrange routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
+  root 'top#index'
+
+  resources :users, only: [:index, :new, :edit]
+
+  resources :proto, only: [:index, :new]
+
+  resources :login, only: [:index]
+
 end
