3c26f438efdb7d90cdf46bb526bc81dbe55d4bcc
ksuematsu <ksuematsu68@gmail.com>
2015-07-18 01:16:32 +0900

    add user_name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712083927) do
+ActiveRecord::Schema.define(version: 20150717161455) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150712083927) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "user_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
