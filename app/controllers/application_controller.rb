a34a293fd88773c5d37d323b40ece39c3adeaa23
ksuematsu <ksuematsu68@gmail.com>
2015-07-18 15:07:29 +0900

    add heads to permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :user_name, :member_of, :profile, :works
+    devise_parameter_sanitizer.for(:sign_up) << [:user_name, :member_of, :profile, :works]
   end
 end
