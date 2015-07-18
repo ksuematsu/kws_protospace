6bd7ff32f1ced062745b353f71efbc59f02e88e9
ksuematsu <ksuematsu68@gmail.com>
2015-07-18 23:10:27 +0900

    wanted to make update function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   def index
   end
+
   def edit
+  end
+
+  def update
+    current_user.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:user_name, :avatar, :email, :password, :member_of, :profile, :works)
   end
 end
