5f77a71acdc33f45bd096fb3078e1af4c1029b79
ksuematsu <ksuematsu68@gmail.com>
2015-07-23 13:54:32 +0900

    set mount uploader

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,8 @@
 class Image < ActiveRecord::Base
   belongs_to :prototype
+
+  mount_uploader :main_image, AvatarUploader
+  mount_uploader :sub_image_1, AvatarUploader
+  mount_uploader :sub_image_2, AvatarUploader
+  mount_uploader :sub_image_3, AvatarUploader
 end
