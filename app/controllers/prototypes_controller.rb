a12f556fb7880085c635d5a2393bdef7b1c04a62
ksuematsu <ksuematsu68@gmail.com>
2015-08-01 17:21:21 +0900

    fixed code alignment

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,8 @@
 class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
-    @comment = Comment.new
-    @comments = @prototype.comments
+    @comment   = Comment.new
+    @comments  = @prototype.comments
   end
   def new
     @prototype = Prototype.new
