96c82a3a26e2cbde909e906872348daf2a6b8bca
ksuematsu <ksuematsu68@gmail.com>
2015-08-02 12:54:02 +0900

    added helper

--- app/helpers/prototypes_helper.rb
+++ app/helpers/prototypes_helper.rb
@@ -2,5 +2,15 @@ module PrototypesHelper
   def by_whom?
     @prototype.user.user_name
   end
+
+  def sub_image(number)
+    if number == 1
+      @prototype.image.sub_image_1
+    elsif number == 2
+      @prototype.image.sub_image_2
+    else
+      @prototype.image.sub_image_3
+    end
+  end
 end
 
