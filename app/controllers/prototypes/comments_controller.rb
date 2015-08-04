739275ac50c4ee6883aed0480bf34fd4f3013160
ksuematsu <ksuematsu68@gmail.com>
2015-08-04 15:12:56 +0900

    nested comments inside of prototypes

---
+++ app/controllers/prototypes/comments_controller.rb
@@ -0,0 +1,13 @@
+class Prototypes::CommentsController < ApplicationController
+  def create
+    @prototype = Prototype.find(params[:prototype_id])
+    @prototype.comments.create(comment_params)
+    @comments  = @prototype.comments
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+  end
+end
+
