bbd8feef5e1500cc29131c16eb1765533e475735
ksuematsu <ksuematsu68@gmail.com>
2015-08-04 15:13:58 +0900

    add module description to routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:new, :create]
 
   resources :prototypes, only: [:show] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create], module: 'prototypes'
   end
 
 end
