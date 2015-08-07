abc7fc60d89fa12ae35050bdb7eea0a109ec9003
ksuematsu <ksuematsu68@gmail.com>
2015-08-07 19:55:23 +0900

    connected views with href information

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.all
+    @tags       = Tag.all
   end
 
   def show
-    @tag = Tag.find(params[:id]).name
-    @prototypes = Prototype.tagged_with(@tag).page(params[:page]).per(4)
+    @tag        = Tag.find(params[:id])
+    @prototypes = Prototype.tagged_with(@tag.name).page(params[:page]).per(4)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def date_of_created(prototype)
-    "#{prototype.created_at.month} , #{prototype.created_at.day}"
+    "#{ prototype.created_at.month } , #{ prototype.created_at.day }"
+  end
+
+  def tag_id(tag)
+    Tag.where(name: "#{tag}")[0].id
   end
 end
