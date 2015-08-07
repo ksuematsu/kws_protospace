4b92a3abab24824820605ad5a9412163eddf6a35
ksuematsu <ksuematsu68@gmail.com>
2015-08-07 19:32:30 +0900

    fixed and set upped page transition

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,7 @@ class TagsController < ApplicationController
   end
 
   def show
-    
+    @tag = Tag.find(params[:id]).name
+    @prototypes = Prototype.tagged_with(@tag).page(params[:page]).per(4)
   end
 end
